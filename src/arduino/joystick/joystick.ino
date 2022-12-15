f8a82c52afdf6f2fb96ac52a1e7cde8baf887d58
Softaren <adrian.hoglund@outlook.com>
2022-12-15 01:31:15 +0100

    added support for joystick X value

--- src/arduino/joystick/joystick.ino
+++ src/arduino/joystick/joystick.ino
@@ -1,17 +1,23 @@
 int VRy = A1;
+int VRx = A0;
 
 int yPosition = 0;
+int xPosition = 0;
 
 void setup() {
   Serial.begin(9600); 
 
   pinMode(VRy, INPUT);  
+  pinMode(VRx, INPUT);
 }
 
 void loop() {
   yPosition = analogRead(VRy);
+  xPosition = analogRead(VRx);
 
-  Serial.println(yPosition);
+  Serial.print(yPosition);
+  Serial.print("|");
+  Serial.println(xPosition);
 
   delay(100);
 }
--- src/controll car python/joystick.py
+++ src/controll car python/joystick.py
@@ -5,9 +5,7 @@ ser = serial.Serial('COM6', 9600, timeout=1)
 time.sleep(2)
 
 def read_joystick():
-
     line = ser.readline()   # read a byte
     if line:
-        string = line.decode()  # convert the byte string to a unicode string
-        num = int(string) # convert the unicode string to an int
-        return num
+        pos = line.decode()  # convert the byte string to a unicode string
+        return pos
