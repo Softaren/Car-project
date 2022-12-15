02fb2a669a91f39dc62ff660dffbd82711bd214e
Softaren <adrian.hoglund@outlook.com>
2022-12-15 04:06:15 +0100

    Added support for two motor groups

--- src/raspberry pi/engine.py
+++ src/raspberry pi/engine.py
@@ -1,31 +1,47 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-dir1 = 3
-dir2 = 5
-enable = 7
+m1_dir1 = 3
+m2_dir1 = 15
+
+m1_dir2 = 5
+m2_dir2 = 13
+
+m1_enable = 7
+m2_enable = 16
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(dir1, GPIO.OUT)
-GPIO.setup(dir2, GPIO.OUT)
+GPIO.setup(m1_dir1, GPIO.OUT)
+GPIO.setup(m1_dir2, GPIO.OUT)
+GPIO.setup(m2_dir1, GPIO.OUT)
+GPIO.setup(m2_dir2, GPIO.OUT)
 
-GPIO.setup(enable, GPIO.OUT)
+GPIO.setup(m1_enable, GPIO.OUT)
+GPIO.setup(m2_enable, GPIO.OUT)
 
-pwm=GPIO.PWM(enable, 100)
+pwm=GPIO.PWM(m1_enable, 100)
+pwm2=GPIO.PWM(m2_enable, 100)
 
 pwm.start(0)
-GPIO.output(enable, True)
+pwm2.start(0)
+GPIO.output(m1_enable, True)
+GPIO.output(m2_enable, True)
 
 def drive(speed):
     # 472 // 479
     if(speed > 479):
         percentage = ((speed - 479) / (1023 - 479)) * 100
-        GPIO.output(3, True)
-        GPIO.output(5, False)
+        GPIO.output(m1_dir1, True)
+        GPIO.output(m1_dir2, False)
+        GPIO.output(m2_dir1, True)
+        GPIO.output(m2_dir2, False)
     else:
         percentage = (1 - (speed / 479)) * 100
-        GPIO.output(3, False)
-        GPIO.output(5, True)
+        GPIO.output(m1_dir1, False)
+        GPIO.output(m1_dir2, True)
+        GPIO.output(m2_dir1, False)
+        GPIO.output(m2_dir2, True)
 
     pwm.ChangeDutyCycle(percentage) # change the duty cycle
+    pwm2.ChangeDutyCycle(percentage) # change the duty cycle
