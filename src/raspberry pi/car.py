2a1268ffdfe407871095e60cc363fc9cbbaabe07
Softaren <adrian.hoglund@outlook.com>
2023-01-06 12:36:28 +0100

    Removed comments

--- src/raspberry pi/car.py
+++ src/raspberry pi/car.py
@@ -1,9 +1,4 @@
 from controller import wait_for_controller_commands
-#from threading import Thread
 
 while True:
-
     wait_for_controller_commands()
-
-    #controller_thread = Thread(target=wait_for_controller_commands)
-    #controller_thread.start()
