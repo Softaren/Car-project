e4f002df767ec411a557e6af2b2b24bbe5b54a30
Softaren <adrian.hoglund@outlook.com>
2023-01-06 10:25:55 +0100

    Added distance sensor

--- src/raspberry pi/engine.py
+++ src/raspberry pi/engine.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
+from distance import distance
 
 m1_dir1 = 3
 m2_dir1 = 15
@@ -31,9 +32,27 @@ def scale_joystick_value(val, src, dst):
     return ((val - src[0]) / (src[1]-src[0])) * (dst[1]-dst[0]) + dst[0]
 
 def drive(c, speed):
+
+    if(distance() < 40):
+        if(speed < 0 or speed > 499):
+            if(c == "j"):
+                percentage = (1 - (speed / 499)) * 100
+                GPIO.output(m1_dir1, False)
+                GPIO.output(m1_dir2, True)
+                GPIO.output(m2_dir1, False)
+                GPIO.output(m2_dir2, True)
+            else:
+                percentage = round(scale_joystick_value(speed, (0, +1), (0, 100)), 2)
+                GPIO.output(m1_dir1, True)
+                GPIO.output(m1_dir2, False)
+                GPIO.output(m2_dir1, True)
+                GPIO.output(m2_dir2, False)
+                sleep(1)
+        return
+
     #Joystick middle
     print(speed)
-    if((speed == 0 and c == "a") or speed == 499):
+    if((speed == 0 and c == "a") or speed == 498):
         pwm.ChangeDutyCycle(0) # change the duty cycle
         pwm2.ChangeDutyCycle(0) # change the duty cycle
         return
