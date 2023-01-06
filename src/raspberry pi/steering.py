021c45aa2e5877620e68a31f3843ad5bf538280e
Softaren <adrian.hoglund@outlook.com>
2023-01-06 12:34:18 +0100

    Removed comment

--- src/raspberry pi/steering.py
+++ src/raspberry pi/steering.py
@@ -11,7 +11,6 @@ def scale_joystick_value(val, src, dst):
     return ((val - src[0]) / (src[1]-src[0])) * (dst[1]-dst[0]) + dst[0]
 
 def steer(c, pos):
-    #527
     if(c == "j"):
         percentage = round(scale_joystick_value(int(pos), (0.0, 1023.0), (-0.24, +0.23)), 2)
     else:
