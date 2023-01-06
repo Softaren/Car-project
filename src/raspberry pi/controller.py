08751539c953ab7a3763dc5c050991c4d97b8d65
Softaren <adrian.hoglund@outlook.com>
2023-01-06 10:21:48 +0100

    Added check for network

--- src/raspberry pi/controller.py
+++ src/raspberry pi/controller.py
@@ -2,14 +2,27 @@ import socket as s
 from engine import drive
 from steering import steer
  
-UDP_IP = "192.168.2.121"
+UDP_IP = "192.168.2.8"
 UDP_PORT = 65432
 
-sock = s.socket(s.AF_INET, s.SOCK_DGRAM)
-sock.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
-sock.bind((UDP_IP, UDP_PORT))
 
-print("Listening... on ", UDP_IP, " with port ", UDP_PORT)
+def is_connected():
+    try:
+        print("Checking network")
+        s.create_connection(("1.1.1.1", 53))
+        return True
+    except OSError:
+        pass
+    return False
+
+while True:
+    if(is_connected()):
+        sock = s.socket(s.AF_INET, s.SOCK_DGRAM)
+        sock.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
+        sock.bind((UDP_IP, UDP_PORT))
+
+        print("Listening... on ", UDP_IP, " with port ", UDP_PORT)
+        break
 
 
 def wait_for_controller_commands():
